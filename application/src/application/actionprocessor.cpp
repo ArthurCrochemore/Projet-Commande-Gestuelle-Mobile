3aac1d0f73e75a6d4b1acca67f56167e3b58ba17
Arthur Crochemore <croa6202@usherbrooke.ca>
2025-04-01 14:52:24 -0400

    feat : Implémentation de la fonction principale

--- application/src/application/actionprocessor.cpp
+++ application/src/application/actionprocessor.cpp
@@ -1,13 +1,31 @@
 #include "./actionprocessor.h"
 
-ActionProcessor::ActionProcessor(IServiceIdentifyAction* action) : action(action) {}
+ActionProcessor::ActionProcessor(uint8_t actionType) {
+    serviceIdentification = IdentifyActionFactory::createIdentifyAction(actionType);
+    action = ExecuteActionCommandFactory::createAction(actionType);
+}
 
+/**
+ * Fonction principale faisant le lien entre le traitement de l'image et l'exécution de l'action
+ * 
+ * @param imagePath : chemin de l'image à traiter
+ * @return : void
+ */
 void ActionProcessor::process(const QString &imagePath) {
-    std::vector<double> image = photoprocessor.process(imagePath);
 
-    // if (action->identify(image)) {
-    //     executeAction();
-    // }
+    qDebug() << "Start ActionProcessor";
+    auto pre = std::chrono::high_resolution_clock::now();
 
-    // TODO : Implémenter correctement
+    qDebug() << "Vectorisation process ...";
+    std::vector<std::vector<greyscale_value>> image = photoprocessor.process(imagePath);
+
+    qDebug() << "Prediction process ...";
+    auto predicted = serviceIdentification->identify(image);
+
+    qDebug() << "Action process ...";
+    action->execute(predicted);
+
+    auto post = std::chrono::high_resolution_clock::now();
+    qDebug() << "Action réalisée en " << QString::number(std::chrono::duration_cast<std::chrono::milliseconds>(post - pre).count()) << " ms";
+
 }
--- application/src/application/actionprocessor.h
+++ application/src/application/actionprocessor.h
@@ -1,18 +1,25 @@
 #include <vector>
+#include <chrono>
 #include <QString>
-#include "./photoprocessor.h"
-#include "./serviceidentify.h"
+
+#include <QDebug> // TODO : retirer
 
 #ifndef ACTIONPROCESSOR_H
 #define ACTIONPROCESSOR_H
 
+#include "./executeactioncommand.h"
+#include "./photoprocessor.h"
+#include "./serviceidentify.h"
+#include "../learning/imageadapter.h"
+
 class ActionProcessor {
 private:
     PhotoProcessor photoprocessor;
-    IServiceIdentifyAction* action;
+    IServiceIdentifyAction* serviceIdentification;
+    IExecuteActionCommand* action;
 
 public:
-    ActionProcessor(IServiceIdentifyAction* action);
+    ActionProcessor(uint8_t actionType);
 
     void process(const QString &imagePath);
 };
